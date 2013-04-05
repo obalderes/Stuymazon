6a024b6b2d975e2911d7f1fd0b7f916186e014d2
NJacob <kn2ply@gmail.com>
2013-04-04 21:43:09 -0400

    a little more

--- db.py
+++ db.py
@@ -2,7 +2,7 @@
 from pymongo import Connection
 
 global con
-con = Connection("mongo.stuycs.org")
+con = Connection("mongo2.stuycs.org")
 global db
 global col
 
@@ -15,5 +15,29 @@ def startup():
 	global col
 	col = db["sales"]
 
+def authuser(username="", password=""):
+	global col
+	res = [x for x in col.find({"entry":"user","user":username, "password":password})]#entries in the db can be user, items, etc
+	if len(res) == 0:
+		return False
+	return True
+
+def newuser(username="", password=""):
+	global col
+	nuser = {"entry":"user","user":str(username), "password":str(password)}
+	res = [x for x in col.find(nuser)]#entries in the db can be user, items, etc
+	if len(res) > 0 or username == "" or password == "":
+		return False
+	col.insert(nuser)
+	return True
+
+def deleteuser(username="", password=""):
+	global col
+	nuser = {"entry":"user","user":str(username), "password":str(password)}
+	res = [x for x in col.find(nuser)]#entries in the db can be user, items, etc
+	if len(res) == 0 or username == "" or password == "":
+		return False
+	col.remove(nuser)
+	return True
 
 
